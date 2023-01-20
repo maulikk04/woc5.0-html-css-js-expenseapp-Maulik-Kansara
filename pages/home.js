3d3942a648e3636db901173e6b86926b30ea8ca1
maulikk04 <118893402+maulikk04@users.noreply.github.com>
2023-01-20 19:44:02 +0530

    home

--- pages/home.js
+++ pages/home.js
@@ -1,26 +1,22 @@
-function v()
-{
+function v() {
     var z = document.getElementById("date").value
-    if(z=="")
-    {
+    if (z == "") {
         alert("Enter Date");
         document.getElementById("date").focus();
         return false;
     }
     var x = document.getElementById("item").value
-    if(x=="")
-    {
+    if (x == "") {
         alert("Enter Item");
         document.getElementById("item").focus();
         return false;
     }
 
     var y = document.getElementById("amt").value
-    if(y=="" || isNaN(y))
-    {
+    if (y == "" || isNaN(y)) {
         alert("Enter Amount");
         document.getElementById("amt").focus();
-        document.getElementById("amt").value="";
+        document.getElementById("amt").value = "";
         return false;
     }
 }
