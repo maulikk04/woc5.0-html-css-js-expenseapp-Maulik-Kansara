421c3a8117cf940fb86b519485d97de14182e09d
maulikk04 <118893402+maulikk04@users.noreply.github.com>
2023-01-28 13:20:48 +0530

    change with js

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,10 @@
-function v() {
+let total = 0;
+let row = 1;
+let index=0;
+function v() 
+{
     var z = document.getElementById("date").value
-    if (z == "") {
+    if (z== "") {
         alert("Enter Date");
         document.getElementById("date").focus();
         return false;
@@ -19,6 +23,36 @@ function v() {
         document.getElementById("amt").value = "";
         return false;
     }
+     var table = document.getElementById('table');
+     row ++;
+     index++;
+     total+=parseFloat(y);
+     var nrow = table.insertRow(row);
+     var c1 = nrow.insertCell();
+     var c2 = nrow.insertCell();
+     var c3 = nrow.insertCell();
+     var c4 = nrow.insertCell();
+     c1.style.textAlign = "center";
+     c2.style.textAlign = "center";
+     c3.style.textAlign = "center";
+     c4.style.textAlign = "center";
+     c1.innerHTML = index;
+     c2.innerHTML = z;
+     c3.innerHTML = y;
+     c4.innerHTML = x;
+     var c5 = document.getElementById('total');
+     c5.innerHTML=total;
+     c5.style.textAlign = "center";
+      r();
+    
+
+} 
+
+function r()
+{
+   document.getElementById("date").value = "" ;
+   document.getElementById("item").value = "" ;
+   document.getElementById("amt").value  = "" ;
 }
 
 
